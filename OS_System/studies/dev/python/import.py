ae420c9324ef3a671e72a160a00830bac66e9b1b
Azrael-S3211 <modderkcaheua@gmail.com>
2024-10-23 15:25:25 -0300

    update

--- OS_System/studies/dev/python/import.py
+++ OS_System/studies/dev/python/import.py
@@ -1,8 +1,25 @@
+from pathlib import Path
 import sys
-import os
 
 # Adiciona o caminho do diretório onde está o arquivo figures.py
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'resource'))
+sys.path.append(str(Path(__file__).resolve().parents[3] / 'resource'))
+
+"""
+Z:\
+└── Users\
+    └── PC\
+        └── Desktop\
+            └── SO.System\
+                └── Projects\
+                    └── Projects_Open\
+                        └── OS_System\
+                            ├── studies\
+                            │   └── dev\
+                            │       └── python\
+                            │           └── import.py
+                            └── resource\
+                                └── figures.py
+"""
 
 # Agora você pode importar tudo do figures.py
 from figures import *
