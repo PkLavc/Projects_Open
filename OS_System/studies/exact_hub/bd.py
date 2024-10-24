2bc9704d21c17328249fe47ba7ebcae2f268dcbd
Azrael-S3211 <modderkcaheua@gmail.com>
2024-10-24 13:59:29 -0300

    update

---
+++ OS_System/studies/exact_hub/bd.py
@@ -0,0 +1,28 @@
+#caracter
+fracao_um_dois = '½'
+velocidade_ao_quadrado = 'v²'
+
+# padrao
+distancia_padrao = 'm' # metros
+tempo_padrao = 's' # segundos
+massa_padrao = 'kg' # quilogramas
+velocidade_padrao = 'm/s' # metros por segundos
+forca_padrao = 'N' # newtons
+energia_trabalho_padrao = 'J' # joules
+constante_elastica_padrao = 'k' # newtons por metro, N/m
+altura_padrao = 'h' # em relação a um ponto de referência (em metros, m)
+
+#formula
+trabalho = 'T=F⋅d⋅cos(θ)'
+velocidade = '''
+  𝑑
+v=―
+  𝑡
+''' # velocidade = distancia / tempo
+forca = 'F=m⋅a'
+
+# energias
+energia_cinetica = 'E=½mv²'
+energia_potencial_elastica = 'E=½kx²' # x é a deformação da mola (em metros, m)
+energia_potencial_gravitacional = 'E=mgh'
+
