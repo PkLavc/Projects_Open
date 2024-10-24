f920f9974665904a3c764527c27f963304880925
Azrael-S3211 <modderkcaheua@gmail.com>
2024-10-24 14:50:34 -0300

    update

--- OS_System/studies/exact_hub/statics_and_dynamics_of_motion/energias.py
+++ OS_System/studies/exact_hub/statics_and_dynamics_of_motion/energias.py
@@ -18,6 +18,7 @@ print('''
 Exercício 2
 
 Um automóvel de 700 kg se desloca a 36 km/h.
+
 a) Qual é a Ec do carro?
 b) O que acontece com a Ec do carro se ele fosse 3 vezes mais leve?
 c) O que acontece com a Ec do carro se ele fosse 2 vezes mais veloz?
@@ -27,19 +28,19 @@ d) O que acontece com a Ec do carro se a direção da velocidade fosse alterada?
 print('''
 Exercício 3
 
-Um corpo de 3,0 kg está em repouso em um colchão de ar horizontal de atrito
-desprezível quando uma força horizontal constante F é aplicada, fazendo-o se
-deslocar por 20 metros com uma velocidade de 3m/s. Qual é a força que foi aplicada
+Um corpo de 3,0 kg está em repouso em um colchão de ar horizontal de atrito desprezível quando uma 
+força horizontal constante F é aplicada, fazendo-o se deslocar por 20 metros com uma velocidade de 3m/s. 
+Qual é a força que foi aplicada
 ao corpo?
 ''') # exercicio 3
 
 print('''
 Exercício 4
 
-Uma caixa de ferramentas é empurrada por 2 m em um piso sujo de óleo (sem atrito)
-de uma oficina de automóveis. A figura mostra o trabalho T realizado sobre a caixa
-pela força horizontal constante em função da posição d da caixa. A escala vertical do
-gráfico é definida por Ts = 6,0 J.
+Uma caixa de ferramentas é empurrada por 2 m em um piso sujo de óleo (sem atrito) de uma oficina 
+de automóveis. A figura mostra o trabalho T realizado sobre a caixa pela força horizontal constante 
+em função da posição d da caixa. A escala vertical do gráfico é definida por Ts = 6,0 J.
+
 a) Qual é o módulo da força?
 b) Se a caixa tivesse uma energia cinética inicial de 3 J, qual seria a energia cinética
 ao final do deslocamento de 2 m?
@@ -48,10 +49,10 @@ ao final do deslocamento de 2 m?
 print(f'''
 Exercício 5
 
-Um trenó e seu ocupante, com massa total de 85 kg, descem uma encosta e
-atingem um trecho horizontal retilíneo com uma velocidade de 37 m/s. Se uma
-força desacelera o trenó até o repouso a uma taxa constante de 2,0 m/s{ao_quadrado},
-determine:
+Um trenó e seu ocupante, com massa total de 85 kg, descem uma encosta e atingem um trecho horizontal 
+retilíneo com uma velocidade de 37 m/s. Se uma força desacelera o trenó até o repouso a uma taxa 
+constante de 2,0 m/s{ao_quadrado}, determine:
+
 a) o módulo F da força
 b) a distância d que o trenó percorre até parar
 c) o trabalho T realizado pela força sobre o trenó
@@ -60,10 +61,10 @@ c) o trabalho T realizado pela força sobre o trenó
 print(f'''
 Exercício 6
 
-A figura mostra três forças aplicadas a um baú que se
-desloca 3 m para a esquerda em um piso sem atrito. Os
-módulos das forças são F{um_para_baixo} = 5 N, F{dois_para_baixo} = 9 N, e F{tres_para_baixo} = 3 N; o
+A figura mostra três forças aplicadas a um baú que se desloca 3 m para a esquerda em um piso sem atrito. 
+Os módulos das forças são F{um_para_baixo} = 5 N, F{dois_para_baixo} = 9 N, e F{tres_para_baixo} = 3 N; o
 ângulo indicado é θ = 60°. No deslocamento,
+
 a) qual é o trabalho total realizado sobre o baú pelas três
 forças?
 b) A energia cinética do baú aumenta ou diminui?
@@ -82,6 +83,7 @@ golfe será maior que a percorrida pela de basquete
 IV. Se ambas colidirem contra um anteparo e pararem, o trabalho realizado pela bola de golfe será igual ao
 realizado pela de basquete
 É correto apenas o que se afirma em
+
 A) I e II.
 B) I e IV.
 C) III e IV.
@@ -115,10 +117,9 @@ aceleração da gravidade igual a 9,8 m/{ao_quadrado}
 print(f'''
 Exercício 2
 
-Dois meninos estão brincando com uma bola de futebol de massa igual a 410 g.
-Um deles joga a bola e acerta uma vidraça. Sabendo que a vidraça se encontra a
-uma altura de 3,0 m do solo, qual o valor da energia potencial da bola ao atingir
-a vidraça? Considere o valor da gravidade local igual a 9,8 m/s{ao_quadrado}.
+Dois meninos estão brincando com uma bola de futebol de massa igual a 410 g. Um deles joga a bola e 
+acerta uma vidraça. Sabendo que a vidraça se encontra a uma altura de 3,0 m do solo, qual o valor da 
+energia potencial da bola ao atingir a vidraça? Considere o valor da gravidade local igual a 9,8 m/s{ao_quadrado}.
 ''') # exercicio 2
 
 print('''
@@ -133,17 +134,16 @@ Exercício 4
 
 Segundo relatos, em 1960, a Sra Maxwell Rogers, da cidade de Tampa, Flórida, EUA, ergueu uma das
 extremidades de um carro que havia caído em cima de seu filho quando um macaco perdeu a sustentação.
-Se, sob pânico, ela efetivamente suspendeu 4000N (cerca de {um_quarto} do peso do veículo) por 5cm, quanto
-trabalho a sua força realizou sobre o carro?
+Se, sob pânico, ela efetivamente suspendeu 4000N (cerca de {um_quarto} do peso do veículo) por 5cm, 
+quanto trabalho a sua força realizou sobre o carro?
 ''') # exercicio 4
 
 print('''
 Exercício 5
 
-No rótulo de uma lata de leite em pó está escrito: valor energético
-1509J por 100 g (361 kcal). Se toda energia armazenada em uma lata
-que contém 400 g de leite for utilizada para levantar um objeto de
-massa 10 kg, qual seria a altura aproximada atingida por este objeto?
+No rótulo de uma lata de leite em pó está escrito: valor energético 1509J por 100 g (361 kcal). 
+Se toda energia armazenada em uma lata que contém 400 g de leite for utilizada para levantar um 
+objeto de massa 10 kg, qual seria a altura aproximada atingida por este objeto?
 a) 25 cm
 b) 15 m
 c) 400 m
@@ -154,10 +154,9 @@ e) 60 m
 print('''
 Exercício 6
 
-Uma corda passa por duas roldanas de massa e atrito
-desprezíveis. Uma caixa de 20kg está pendurada em uma
-das roldanas. Se for exercida uma força F sobre a
-extremidade livre da corda,
+Uma corda passa por duas roldanas de massa e atrito desprezíveis. Uma caixa de 20kg está 
+pendurada em uma das roldanas. Se for exercida uma força F sobre a extremidade livre da corda,
+
 a) Qual deve ser a intensidade de F a fim de que a caixa
 seja suspensa com velocidade constante?
 b) Para suspender a caixa 2cm, em quantos centímetros
@@ -171,11 +170,10 @@ realizado sobre a caixa,
 print('''
 Exercício 7
 
-Um bloco de gelo de 45kg desce deslizando por um plano
-inclinado liso de 1,5m de comprimento e 91cm de altura.
-Um trabalhador aplica uma força para cima contra o bloco
-de gelo na direção paralela ao plano inclinado, para que o
-bloco desça deslizando com velocidade constante.
+Um bloco de gelo de 45kg desce deslizando por um plano inclinado liso de 1,5m de comprimento 
+e 91cm de altura. Um trabalhador aplica uma força para cima contra o bloco de gelo na direção 
+paralela ao plano inclinado, para que o bloco desça deslizando com velocidade constante.
+
 a) Encontre a intensidade da força do trabalhador
 b) Quanto trabalho é realizado sobre o bloco
     i. pela força do trabalhador?
@@ -187,23 +185,16 @@ b) Quanto trabalho é realizado sobre o bloco
 print('''
 Exercício 8
 
-Você deixa cair um livro de 2 kg para uma amiga que está
-na calçada, a uma distância D = 10 m abaixo de você. Se as
-mãos estendidas da sua amiga estão a uma distância d =
-1,5 m acima do solo,
-a) qual é o trabalho realizado sobre o livro pela força
-gravitacional até o livro cair nas mãos da sua amiga?
-b) Se a energia potencial gravitacional U do sistema é
-considerada nula no nível do solo, qual é o valor de U
+Você deixa cair um livro de 2 kg para uma amiga que está na calçada, a uma distância D = 10 m 
+abaixo de você. Se as mãos estendidas da sua amiga estão a uma distância d = 1,5 m acima do solo,
+
+a) qual é o trabalho realizado sobre o livro pela força gravitacional até o livro cair nas mãos da sua amiga?
+b) Se a energia potencial gravitacional U do sistema é considerada nula no nível do solo, qual é o valor de U
     i. quando você deixa cair o livro
     ii. quando o livro chega às mãos da sua amiga?
-c) Suponha agora que o referencial de energia é 100 J ao
-nível do solo. Recalcule
-    i. o trabalho realizado sobre o livro pela força
-    gravitacional até o livro cair nas mãos da sua
-    amiga
-    ii. A energia potencial gravitacional do livro nas
-    mãos de sua amiga
+c) Suponha agora que o referencial de energia é 100 J ao nível do solo. Recalcule
+    i. o trabalho realizado sobre o livro pela força gravitacional até o livro cair nas mãos da sua amiga
+    ii. A energia potencial gravitacional do livro nas mãos de sua amiga
 ''') # exercicio 8
 
 print('''
@@ -225,9 +216,10 @@ print('\t\t\t\tLista de Exercícios - 5 (Energia Potencial Elástica)')
 print('''
 Exercício 1
 
-Um bloco de 250 g é deixado cair em uma mola vertical, inicialmente relaxada, cuja constante elástica é k =
-2,5 N/cm. O bloco fica acoplado à mola, comprimindo-a em 12 cm até parar momentaneamente. Nessa
+Um bloco de 250 g é deixado cair em uma mola vertical, inicialmente relaxada, cuja constante elástica 
+é k = 2,5 N/cm. O bloco fica acoplado à mola, comprimindo-a em 12 cm até parar momentaneamente. Nessa
 compressão, que trabalho é realizado sobre o bloco
+
 a) pela força gravitacional?
 b) pela força elástica?
 c) Com que força o bloco comprime a mola?
@@ -236,8 +228,8 @@ c) Com que força o bloco comprime a mola?
 print('''
 Exercício 2
 
-Calcule a energia potencial elástica armazenada em uma mola, cuja constante elástica é 100 N/m, que está
-comprimida, apresentando uma deformação de 45 cm.
+Calcule a energia potencial elástica armazenada em uma mola, cuja constante elástica é 100 N/m, que 
+está comprimida, apresentando uma deformação de 45 cm.
 ''') # exercicio 2
 
 print('''
@@ -255,9 +247,10 @@ e) 2,0J
 print('''
 Exercício 4
 
-Quando um gatilho é desarmado, a mola da figura, que está
-comprimida de 20cm, lança a esfera de massa 0,4kg para a direita.
+Quando um gatilho é desarmado, a mola da figura, que está comprimida de 20cm, lança a esfera de massa 
+0,4kg para a direita.
 Supondo desprezível o atrito e a constante da mola igual a 250 N/m,
+
 a) Qual a força que o gatilho está exercendo no sistema?
 b) Qual a capacidade de trabalho do sistema? 
 ''') # exercicio 4
